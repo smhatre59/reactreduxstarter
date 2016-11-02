f3187ce3b41f9660e573c15fd3357533948bede6
Saurabh Mhatre <smhatre59@gmail.com>
2016-11-02 23:15:20 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import {Provider} from "react-redux";
 //components
-import MainComponent from "./components/main.js";
+import MainComponent from "./components/Main.js";
 
 ReactDOM.render(
                 <MainComponent />
